088e53b05961c304efe017b06600488b512d7617
Dmitry Lebedev <dmitry_lebedev1@epam.com>
2015-10-21 15:48:30 +0300

    f2

--- src/main/resources/scr2.js
+++ src/main/resources/scr2.js
@@ -17,11 +17,45 @@ window.addEventListener('load', function(){
         for (item of pages) {
             res.pages.push(JSON.parse(Cookies.get("/HTMLParse/" + item)));
         }
-        str = JSON.stringify(res);
-        Cookies.set(location.pathname, res);
-        console.log(str);
+        str = JSON.stringify(res, undefined, 4);
+        openResults(str);
     }
 });
+
+function openResults(data){
+    win = window.open("", "Result JSON", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=780, height=200, top="+(screen.height-400)+", left="+(screen.width-840));
+
+    win.document.head.innerHTML = "<style> \
+                                       pre {outline: 1px solid #ccc; padding: 5px; margin: 5px; } \
+                                       .string { color: green; } \
+                                       .number { color: darkorange; } \
+                                       .boolean { color: blue; } \
+                                       .null { color: magenta; } \
+                                       .key { color: red; } \
+                                   </style>"
+    win.document.body.appendChild(document.createElement('pre')).innerHTML = syntaxHighlight(data);
+}
+
+
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
 
 function getCount(){
     i = Cookies.get("count");
